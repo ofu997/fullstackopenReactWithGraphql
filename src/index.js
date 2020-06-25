f165c55a1353d0dc842bad74c97d766fe4540564
ofu997 <ofu997@gmail.com>
2020-06-24 19:37:38 -0700

    add apollo client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+import { ApolloClient, ApolloProvider, gql, HttpLink, InMemoryCache } from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const query = gql`
+query {
+  allPersons  {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
